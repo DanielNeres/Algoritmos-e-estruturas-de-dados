ec1eacaf09ad93df37c4ae08d117ebac2ee2a0f3
DanielNeres <danielnerres@gmail.com>
2023-12-06 19:41:40 -0300

    parte 3 Aula 4|12 V.1.2

--- Aula 4do12/parte 3 Aula 4do12.c
+++ Aula 4do12/parte 3 Aula 4do12.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
-float soma(int a, int b){
-return a + b;
-}
-float subtracao(int a, int b){
-return a - b;
-}
-float produto(int a, int b){
-return a * b;
-}
-float divisao(int a, int b){
-return a / b;
+
+float soma(float a, float b) {
+    return a + b;
 }
 
-float calcula(int x, int y, float (*operacao)(int, int)){
-return (*operacao)(x, y);
+float subtracao(float a, float b) {
+    return a - b;
 }
 
-int main(){
-float resultado = calcula(5, 2, divisao);
+float produto(float a, float b) {
+    return a * b;
+}
 
-printf("resultado: %.2f", resultado);
-return 0;
+float divisao(float a, float b) {
+    return (float)a / b;
+}
+float calcula(int x, int y, float (*operacao)(float, float)) {
+    return (*operacao)(x, y);
+}
+
+int main() {
+    float resultado = calcula(5, 2, produto);
+
+    printf("resultado: %.2f", resultado);
+    return 0;
 }
