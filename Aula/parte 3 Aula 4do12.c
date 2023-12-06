e1457da94c785ff778d480faa2fb608ebba0eddd
DanielNeres <danielnerres@gmail.com>
2023-12-06 19:37:04 -0300

    parte 3 Aula 4|12

---
+++ Aula/parte 3 Aula 4do12.c
@@ -0,0 +1,21 @@
+int soma(int a, int b){
+return a + b;
+}
+int subtracao(int a, int b){
+return a - b;
+}
+int produto(int a, int b){
+return a * b;
+}
+
+
+int calcula(int\ x, int y, int (*operacao)(int, int)){
+return (*operacao)(x, y);
+}
+
+int main(){
+int resultado  = calcula (5, 3, soma);
+
+printf("resultado: %d", resultado);
+return 0;
+}
