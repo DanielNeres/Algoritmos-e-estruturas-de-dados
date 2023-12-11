3fa5dc54cc5051fb67e95c7dbf3fad9757a030cd
DanielNeres <danielnerres@gmail.com>
2023-12-11 14:27:47 -0300

    parte 1 V.1

---
+++ Aula 11do12/Parte 1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+    struct aluno
+    {
+        char nome[20];
+        int idade;
+        int mat;
+        char email[50];
+    };
+    struct aluno a;
+    a.mat = 202210124;
+    printf("%d", a.mat);
+    return 0;
+}
