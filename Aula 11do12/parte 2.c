a097728b43006e782f90eee6fbc0a37e310985cc
DanielNeres <danielnerres@gmail.com>
2023-12-11 15:04:59 -0300

    parte 2 V.3

--- Aula 11do12/parte 2.c
+++ Aula 11do12/parte 2.c
@@ -6,12 +6,19 @@ int idade;
 int mat;
 char email[50];
 };
+void preencher(struct aluno *estudante);
 
 int main(){
     struct aluno * estudante = (struct aluno*) malloc(sizeof(struct aluno));
     if(estudante == NULL){
         exit(1);
     }
+    preencher(estudante);
+    free(estudante);
+    return 0;
+}
+
+void preencher(struct aluno *estudante){
     printf("%d\n", (*estudante).mat);
     printf("digite o nome do estudante\n");
     scanf("%[^\n]", (*estudante).nome);
@@ -21,6 +28,4 @@ int main(){
     scanf("%d", &(*estudante).mat);
     printf("digite o email do estudante\n");
     scanf(" %[^\n]", (*estudante).email);
-    free(estudante);
-    return 0;
 }
