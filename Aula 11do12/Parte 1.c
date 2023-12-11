4d403e310736581949a63d380553bdfa697e54c9
DanielNeres <danielnerres@gmail.com>
2023-12-11 14:43:14 -0300

    parte 1 V.2

--- Aula 11do12/Parte 1.c
+++ Aula 11do12/Parte 1.c
@@ -8,9 +8,17 @@ int main(){
         int mat;
         char email[50];
     };
-    struct aluno a;
-    a.mat = 202210124;
-    a.idade = 20;
-    printf("%d", a.mat);
+    struct aluno estudante;
+    estudante.mat = 202210124;
+    estudante.idade = 20;
+    printf("%d\n", estudante.mat);
+    printf("digite o nome do estudante\n");
+    scanf("%[^\n]", estudante.nome);
+    printf("digite a idade do estudante\n");
+    scanf("%d", &estudante.idade);
+    printf("digite a matricula do estudante\n");
+    scanf("%d", &estudante.mat);
+    printf("digite o email do estudante\n");
+    scanf(" %[^\n]", estudante.email);
     return 0;
 }
