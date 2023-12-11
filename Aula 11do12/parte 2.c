9139dff3ea5d5debeb83f49bb6d6e7c79f7c45ac
DanielNeres <danielnerres@gmail.com>
2023-12-11 14:54:03 -0300

    parte 2 V.2

--- Aula 11do12/parte 2.c
+++ Aula 11do12/parte 2.c
@@ -9,5 +9,18 @@ char email[50];
 
 int main(){
     struct aluno * estudante = (struct aluno*) malloc(sizeof(struct aluno));
+    if(estudante == NULL){
+        exit(1);
+    }
+    printf("%d\n", (*estudante).mat);
+    printf("digite o nome do estudante\n");
+    scanf("%[^\n]", (*estudante).nome);
+    printf("digite a idade do estudante\n");
+    scanf("%d", &(*estudante).idade);
+    printf("digite a matricula do estudante\n");
+    scanf("%d", &(*estudante).mat);
+    printf("digite o email do estudante\n");
+    scanf(" %[^\n]", (*estudante).email);
+    free(estudante);
     return 0;
 }
