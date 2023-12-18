3f27d3721e4c4d82c2a4ee9e8c3004ff38b4fb32
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-12-18 17:02:03 -0300

    Aula 18do12 comentado

--- Aula 18do12.c
+++ Aula 18do12.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// criação da struct dependente
 typedef struct disciplina{
     char nome[20];
     float nota;
     int codigo;
 }Disciplina;
 
+// criação da struct principal
 typedef struct aluno{
     char nome[20];
     int idade;
@@ -15,22 +17,24 @@ typedef struct aluno{
 }Aluno;
 
 int main(void){
+    // criação da variavel ponteiro vetor aluno
     Aluno *aluno = (Aluno*) malloc(sizeof(Aluno)); 
     if(aluno == NULL){
         exit(1);
     }
-
+    // definição da quantidade de materias atravez da alocação dinamica malloc
     aluno -> materias = (Disciplina*) malloc(2*sizeof(Disciplina));
     if(aluno -> materias == NULL){
         exit(1);
     }
-
+    // coleta as informações do aluno
     printf("informe os dados do aluno: \n");
     scanf(" %[^\n]", aluno -> nome);
     scanf("%d %d", &aluno->idade, &aluno->matricula);
 
     int index;
 
+    // coleta as informações das materias
     for(index = 0; index < 2; index++){
         printf("Cadastro de disciplinas %d : \n", index+1);
         printf("Informe o nome: \n");
@@ -40,14 +44,12 @@ int main(void){
         printf("Informe o codigo: \n");
         scanf("%d", &aluno->materias[index].codigo);
     }
-
+    // imprimi as informações das diciplinas
     for(index = 0; index < 2; index++){
         printf("Disciplina %d : \n", index+1);
-        printf("Nome: %s\nNota: %f\nCodigo: %d\n", aluno->materias[index].nome, aluno->materias[index].nota,
+        printf("Nome: %s\nNota: %.2f\nCodigo: %d\n", aluno->materias[index].nome, aluno->materias[index].nota,
         aluno->materias[index].codigo);
     }
-
-    //printf("%d %d %d \n", aluno.idade, aluno.matricula, aluno.materias.codigo);
 
 
     return 0;
